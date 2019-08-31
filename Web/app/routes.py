bdd0a835375ccfbf5b7b9ea3879d0366c21f2178
Amarnath Karthi <97amarnathk@gmail.com>
2019-08-31 22:43:56 +0530

    Add result and form inputs

--- Web/app/routes.py
+++ Web/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, Flask, request, redirect, url_for, jsonify
 from app import app
 
 @app.route('/')
@@ -6,3 +6,13 @@ from app import app
 def index():
     user = {'username':'Anonymous'}
     return render_template('index.html', title='Home', user=user)
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method=='GET':
+        return render_template('userform.html', title='Home')
+    if request.method=='POST':
+        for k in request.form.keys():
+            print(request.form)
+            result = predict(request.form)
+        return render_template("result.html", prob_yes = result)
