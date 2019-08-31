924557fc73f0303b1d7c4f747b374eddbc29ecb4
Pratik Padalia <pratik.padalia@akridata.com>
2019-09-01 05:08:53 +0530

    updated ML part

--- app/prediction_model/TreatmentPrediction.py
+++ app/prediction_model/TreatmentPrediction.py
@@ -16,7 +16,30 @@ from sklearn import metrics
 import numpy as np
 import pickle
 
-dataset = pd.read_csv(r'./app/prediction_model/trainms.csv',parse_dates=['Timestamp'],index_col='s.no')
+def fit(data_list):
+    """
+    This will fit the encoder for all the unique values and introduce unknown value
+    :param data_list: A list of string
+    :return: self
+    """
+    label_encoder = LabelEncoder()
+    label_encoder = label_encoder.fit(list(data_list) + ['Unknown'])
+    return label_encoder
+
+def transform(label_encoder, data_list):
+    """
+    This will transform the data_list to id list where the new values get assigned to Unknown class
+    :param data_list:
+    :return:
+    """
+    new_data_list = list(data_list)
+    for unique_item in np.unique(data_list):
+        if str(unique_item) not in label_encoder.classes_:
+            new_data_list = ['Unknown' if x==unique_item else x for x in new_data_list]
+
+    return label_encoder.transform(new_data_list)
+
+dataset = pd.read_csv(r'./app/prediction_model/trainData.csv',parse_dates=['Timestamp'],index_col='s.no')
 dataset = dataset.drop(['Timestamp'],axis=1)
 
 dataset= dataset.replace(np.nan, '', regex=True)
@@ -25,13 +48,15 @@ dataset= dataset.replace(np.nan, '', regex=True)
 y = dataset.treatment
 x = dataset.drop('treatment',axis=1)
 
+print(x.shape)
+
 #labeling
 idx = 0
 for col in x.columns:
-    label_encoder = LabelEncoder()
     print("{} {}".format(idx, col))
     idx += 1
-    x[col]=label_encoder.fit_transform(x[col])
+    label_encoder = fit(x[col])
+    x[col]=transform(label_encoder, x[col])
     outfile = './app/prediction_model/columns/' + col
     with open(outfile, 'wb') as pickle_file:
         pickle.dump(label_encoder, pickle_file)
@@ -41,14 +66,14 @@ for col in x.columns:
 ## 3. fit 
 #logreg.fit(x, y)
 
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.00001)
 
 #x_train.head()
 #x_train.shape
 #x_test.head()
 #x_test.shape
 
-trained_model = RandomForestClassifier().fit(x,y)
+trained_model = RandomForestClassifier(n_estimators=100).fit(x,y)
 
 outfile = './app/prediction_model/prediction.model'
 with open(outfile, 'wb') as pickle_file:
