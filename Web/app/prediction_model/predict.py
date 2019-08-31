48c609d4723b30ad4c4bc5e1002def456e638097
Amarnath Karthi <97amarnathk@gmail.com>
2019-09-01 00:47:07 +0530

    Return prediction probability

--- Web/app/prediction_model/predict.py
+++ Web/app/prediction_model/predict.py
@@ -19,8 +19,8 @@ def treatment_prediction(input_dict):
 
     # Use the loaded model to make predictions 
     y_pred = trained_model.predict(x)
-    return y_pred
+    return y_pred, trained_model.predict_proba(x)
 
 if __name__ == '__main__':
     input_dict = {'Age':[25],'Gender':['Male'],'Country':['United States'],'state':['PA'],'self_employed':['No'],'family_history':['Yes'],'work_interfere':['Often'],'no_employees':['6-25'],'remote_work':['Yes'],'tech_company':['Yes'],'benefits':['Yes'],'care_options':['Yes'],'wellness_program':['No'],'seek_help':["Don't know"],'anonymity':['Yes'],'leave':['Somewhat easy'],'mental_health_consequence':['No'],'phys_health_consequence':['No'],'coworkers':['Yes'],'supervisor':['Yes'],'mental_health_interview':['Maybe'],'phys_health_interview':['Maybe'],'mental_vs_physical':["Don't know"],'obs_consequence':["No"],'comments':[""]}
-    main()
+    print(treatment_prediction(input_dict))
